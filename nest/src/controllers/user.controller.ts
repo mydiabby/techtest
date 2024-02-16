34fa4ec45d7d47d69dba9e15e4bb4328529e6c74
Gilles COPPE <gilles.coppe@smartcom.com>
2024-02-16 17:19:54 +0100

    Add response status for some method

--- nest/src/controllers/user.controller.ts
+++ nest/src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, HttpException, HttpStatus } from '@nestjs/common';
+import { ApiCreatedResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOkResponse, ApiParam } from '@nestjs/swagger';
 import { GetFullNamesOfAllUsers } from 'src/application/use-cases/getFullNameOfAllUsers';
 import { User } from 'src/domain/entities/user';
 
@@ -9,19 +10,31 @@ export class UserController {
   ) {}
 
   @Get('list')
+  @ApiOkResponse({ status: 200, description: 'User\'s list returned successfully.' })
   async getFullNamesOfAllUsers(): Promise<string[]> {
     return await this.useCase.getFullName();
   }
 
   @Get()
+  @ApiOkResponse({ status: 200, description: 'Users returned successfully.' })
   async getAll(): Promise<User[]> {
     return await this.useCase.getUsers();
   }
 
   @Get(':id')
+  @ApiParam({ name: 'id', description: 'User ID' })
+  @ApiOkResponse({ status: 200, description: 'User found successfully.' })
+  @ApiNoContentResponse({ status: 204, description: 'User not found.' })
   async findById(@Param('id') id: string): Promise<User> {
-    return this.useCase.findById(Number(id));
+    const user = await this.useCase.findById(Number(id));
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NO_CONTENT);
+    }
+    return user;
   }
+  // async findById(@Param('id') id: string): Promise<User> {
+  //   return this.useCase.findById(Number(id));
+  // }
 
   @Post()
   async create(@Body() user: User): Promise<User> {
