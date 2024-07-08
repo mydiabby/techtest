eccf917b6d026d2464c904196c122c4e9c219b79
Jouet <thibaut.jouet64@gmail.com>
2024-07-08 17:50:44 +0200

    Resolved small problem in user display and the way they got fetched

--- angular/src/app/list-users/list-users.component.ts
+++ angular/src/app/list-users/list-users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 
-type User = {
+export type User = {
   id:string,
   firstName:string,
   lastName:string,
@@ -19,25 +19,25 @@ type User = {
 })
 export class ListUsersComponent implements OnInit {
   
+  
   userList: User[] = []
 
-  ngOnInit(): void { // = useEffect en react 
-      this.getUsers().then((res: User[]) => {
-        this.userList = res
-      })
-  }
-
   async getUsers (): Promise<User[]> {
-    // use services.
+    // TODO: use services.
     try {
-      const response = await fetch ("http://localhost:3000/users") 
+      const response = await fetch ("http://localhost:3000/users") // env v
       const users = await response.json()
-      console.log("users",users)
-      return users?.body as User[]
+
+      return users as User[]
     } catch (e) {
       console.log("e", e)
     }
     return []
   }
 
+  ngOnInit(): void {
+    this.getUsers().then((res: User[]) => {
+      this.userList = res
+      console.log(this.userList)
+    })}
 }
