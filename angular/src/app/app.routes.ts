fcd21c67141ccba6229dfc56368698d3fed35a61
Gaétan Juste <gjuste@student.42.fr>
2026-05-07 02:38:20 +0200

    feat(angular): add cosmetics and responsive

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -3,16 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    title: 'myDiabby',
     loadComponent: () =>
       import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'users',
+    title: 'Utilisateurs - myDiabby',
     loadComponent: () =>
       import('./pages/users/users.component').then((m) => m.UsersComponent),
   },
   {
     path: 'users/add',
+    title: 'Ajouter un utilisateur - myDiabby',
     loadComponent: () =>
       import('./pages/add-user/add-user.component').then(
         (m) => m.AddUserComponent,
